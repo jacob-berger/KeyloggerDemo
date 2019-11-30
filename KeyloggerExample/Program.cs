28d4e0e1f9e37f6e43f52d2588b065c98682909b
Jacob Berger <jberger.1919@gmail.com>
2019-11-29 17:27:23 -0800

    Initial

---
+++ KeyloggerExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//https://null-byte.wonderhowto.com/how-to/create-simple-hidden-console-keylogger-c-sharp-0132757/
+namespace KeyloggerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
